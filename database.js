628ea0531890ce56aa3d2bba665dff93fc97103f
Ru <105895985+aenastasia@users.noreply.github.com>
2022-05-20 12:19:25 +0700

    First upload

---
+++ database.js
@@ -0,0 +1,103 @@
+let menu = [];
+
+let menu_detail = {
+foto: "https://images.bisnis-cdn.com/thumb/posts/2021/03/18/1369483/minuman-sehat-buah-its-buah.jpg?w=744&h=465",
+harga: 15000,
+kode: "BRG001",
+nama: "Jus",
+rating: 5.0,
+stock: 10,
+};
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://cdn0-production-images-kly.akamaized.net/aDZbOOV2WeJCgCiBFgFQwoXJN64=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2029093/original/066661300_1521949447-Bakso.jpg",
+    harga: 15000,
+    kode: "BRG002",
+    nama: "Bakso",
+    rating: 5.0,
+    stock: 20,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://www.madiunpos.com/files/2020/03/HeciInstagram.kreasimasakanrumah.jpg",
+    harga: 2000,
+    kode: "BRG003",
+    nama: "Heci",
+    rating: 5.0,
+    stock: 100,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://asset.kompas.com/crops/gHb34PIgwftVEw2dqbqT2u90gsw=/0x0:1000x667/780x390/data/photo/2020/10/05/5f7a28ca1820d.jpg",
+    harga: 500,
+    kode: "BRG004",
+    nama: "Permen",
+    rating: 5.0,
+    stock: 1000,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://images.tokopedia.net/img/cache/500-square/product-1/2020/1/3/84690176/84690176_2c64dd23-fe8c-404f-b076-10499d3398b8_690_690.jpg",
+    harga: 10000,
+    kode: "BRG005",
+    nama: "Air Mineral",
+    rating: 5.0,
+    stock: 30,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://cdn1-production-images-kly.akamaized.net/ysEENzy8zUcwS8ZbarCyKtc9UfA=/88x216:2571x1615/469x260/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3604141/original/057889500_1634358501-bakso_aci_kuah_pedas.jpg",
+    harga: 10000,
+    kode: "BRG006",
+    nama: "Baso Aci",
+    rating: 5.0,
+    stock: 25,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://img-global.cpcdn.com/recipes/d673616eeeb1cbf5/1200x630cq70/photo.jpg",
+    harga: 15000,
+    kode: "BRG007",
+    nama: "Gado-Gado",
+    rating: 5.0,
+    stock: 40,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://asset.kompas.com/crops/zPKBu9dQmOtq8gR0Lpo2lZItBVs=/0x227:800x760/750x500/data/photo/2021/07/27/60ffc032c141b.jpg",
+    harga: 15000,
+    kode: "BRG008",
+    nama: "Boba",
+    rating: 5.0,
+    stock: 50,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://asset-a.grid.id/crop/0x0:0x0/360x240/photo/2020/10/09/45410340.jpg",
+    harga: 20000,
+    kode: "BRG009",
+    nama: "Terang Bulan",
+    rating: 5.0,
+    stock: 60,
+    };
+menu.push(menu_detail);
+
+menu_detail = {
+    foto: "https://awsimages.detik.net.id/community/media/visual/2021/07/10/resep-chicken-wings-ala-korea_43.jpeg?w=650&q=80",
+    harga: 14000,
+    kode: "BRG0010",
+    nama: "Chicken Wings",
+    rating: 5.0,
+    stock: 60,
+    };
+menu.push(menu_detail);
+
+console.log(menu)
